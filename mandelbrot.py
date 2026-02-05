859c7d1fed0af7738207a07bc1f3ad6eed4b2d04
SnorreJohnsen <snorrejohnsen@gmail.com>
2026-02-05 15:06:36 +0100

    added mandelbrot point loop function

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,18 +3,34 @@ Mandelbrot Set Generator
 Author : [ Snorre Johnsen ]
 Course : Numerical Scientific Computing 2026
 """
-def f ( x ) :
-"""
-Example function .
-Parameters
-----------
-x : float
-Input value
-Returns
--------
-float
-Output value
-"""
-# TODO : Implement the algorithm
-# small change for test
-pass
+
+
+
+def mandelbrot_point ( c ) :
+    """
+    Example function .
+    Parameters
+    ----------
+    c : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+
+    z = 0 
+    max_iter = 100
+
+    for n in range(max_iter):
+        z = z**2 + c
+        if abs(z) > 2:
+            return n
+    return max_iter
+
+
+if __name__ == "__main__":
+    c = 0
+    n = mandelbrot_point(c)
+
+    print(f"{c=}", "and iterations =", n)
